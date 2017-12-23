fab94a150902ce449eba24bbc2340e151afd8cd2
Carmine DiMascio <dimascio@us.ibm.com>
2017-12-23 01:47:39 -0500

    add github button

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,9 +2,7 @@
   <div class="container">
     <md-toolbar class="md-dense">
       <h2 class="md-title" style="flex: 1">Story Enchrichment (demo)</h2>
-      <md-button class="md-icon-button">
-        <md-icon>search</md-icon>
-      </md-button>
+      <a class="github-button" href="https://github.com/cdimascio/nlu-news-web" data-icon="octicon-star" data-size="large" aria-label="Star cdimascio/nlu-news-web on GitHub">Star</a>
     </md-toolbar>
   </div>
 </template>
