4150df1456c630795d6e107d0406477150a0171c
Carmine DiMascio <dimascio@us.ibm.com>
2017-12-26 17:20:56 -0500

    update dependencies

--- src/services/nlu.js
+++ src/services/nlu.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import dps from 'dbpedia-sparql-client';
+import dps from 'dbpedia-sparql-client'
 import { nluNewsApiRoot } from '../config'
 
 const extract = _ => _.data
